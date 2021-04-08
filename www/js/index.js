030ff7faa479e9b682e40186c7f532c93bac54a8
YoannaKuneva <79211246+YoannaKuneva@users.noreply.github.com>
2021-04-08 08:12:32 +0300

    Commit

--- www/js/index.js
+++ www/js/index.js
@@ -4,5 +4,13 @@ function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+    navigator.geolocation.watchPosition(geolocationSuccess,geolocationError);
+}
+
+function geolocationSuccess(position) {
+    $('#latitude').text(position.coords.latitude);
+}
+
+function geolocationError(error) {
+    alert(error);    
 }
